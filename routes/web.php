f9c72407e66dfe3ea471bfd8f3b9bcd331923730
FreshDev <billyhans90@gmail.com>
2021-12-28 06:16:46 +0100

    Multi Auth with admin guard part 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+/* ------------ Admin Route ---------- */
+
+Route::prefix('admin')->group(function() {
+
+    Route::get('/login',[AdminController::class, 'Index'])->name('login_form');
+
+    Route::get('/login/owner',[AdminController::class, 'Login'])->name('admin.login');
+
+    Route::get('/dashboard',[AdminController::class, 'Dashboard'])->name('admin.dashboard');
+
+});
+
+
+/* ------------ End Admin Route ---------- */
+
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
